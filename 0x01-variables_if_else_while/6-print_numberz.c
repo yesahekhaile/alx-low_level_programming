ce67e15c92ffaf3116c2c05a5f8c9f8d173b769f
yesahekhaile <yesahekhaile@gmail.com>
2022-03-11 23:21:17 +0300

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Everything run here
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int num = '0';
+while (num <= '9')
+{
+putchar(num);
+num++;
+}
+putchar('\n');
+return (0);
+}
