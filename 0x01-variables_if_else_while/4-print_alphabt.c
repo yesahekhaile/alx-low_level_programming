3c01c5b2651e28581c4ed8fe35818982f7e5a477
yesahekhaile <yesahekhaile@gmail.com>
2022-03-11 22:49:08 +0300

    print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x == 'q' || x == 'e')
+		{
+		continue;
+		}
+		putchar(x);
+	}
+	putchar(10);
+	return (0);
+}
