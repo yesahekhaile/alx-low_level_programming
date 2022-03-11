5edff8914640242eac6cfbd0a1647abdfb735069
yesahekhaile <yesahekhaile@gmail.com>
2022-03-11 21:22:46 +0300

    print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		y = tolower(x);
+		putchar(y);
+	}
+	return (0);
+}
