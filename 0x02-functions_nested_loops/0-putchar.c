90ba914e2ce7d58fabb836e017850c3af4eb0580
yesahekhaile <yesahekhaile@gmail.com>
2022-03-15 22:50:28 +0300

    _putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - This is the main function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char b[9] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 8; i++)
+	{
+	_putchar(b[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
+
