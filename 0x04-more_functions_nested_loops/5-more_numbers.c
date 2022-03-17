781c93818ef85b621fdccd9ae27cba61a0804a0d
yesahekhaile <yesahekhaile@gmail.com>
2022-03-17 22:21:21 +0300

    more numbers and print lines

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the numbers, from 0 to 14 , 10 times
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+	for (j = 0; j < 15; j++)
+	{
+	if (j >= 10)
+	_putchar(j / 10 + '0');
+	_putchar(j % 10 + '0');
+	}
+	_putchar('\n');
+}
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - prints lines.
+ * @n: variabel recived from main.
+ */
+
+void print_line(int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
